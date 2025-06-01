ed429e232eae8534bdda61c82ddeef0dd5a31377
dang0x5f <d4n9xcv@gmail.com>
2025-06-01 14:16:09 -0400

    https://codeforces.com/problemset/problem/959/A

---
+++ codeforces/CODEFORCES_959_a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int n;
+    scanf("%d",&n)&&getchar();//\n
+    if(n&1)
+        printf("Ehab\n");
+    else
+        printf("Mahmoud\n");
+    return(0);
+}
