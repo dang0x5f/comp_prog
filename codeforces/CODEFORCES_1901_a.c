311efbde67b9fbc5913cc5ac8862442c41214e0e
dang0x5f <d4n9xcv@gmail.com>
2025-06-08 19:21:32 -0400

    https://codeforces.com/problemset/problem/1901/A

--- codeforces/CODEFORCES_1901_a.c
+++ codeforces/CODEFORCES_1901_a.c
@@ -4,17 +4,16 @@ void solve(void)
 {
     int n,x;
     scanf("%d %d",&n,&x)&&getchar();
-    int ns[n+2];
-    ns[0] = 0;
-    ns[n-1] = x;
+    int ns[n+1];
+    ns[n] = x;
     for(int i=0; i<n; ++i)
         scanf("%d",&ns[i])&&getchar();
-    int min_liter = ns[1], dst;
-    for(int i=0; i<n; ++i){
-        if(i == n-1){
-            dst = (ns[i+1]-ns[i])*2;
+    int min_liter = ns[0], dst;
+    for(int i=1; i<=n; ++i){
+        if(i == n){
+            dst = (ns[i]-ns[i-1])*2;
         }else{
-            dst = (ns[i+1]-ns[i]);
+            dst = (ns[i]-ns[i-1]);
         }
         min_liter = MAX(min_liter,dst);
     }
