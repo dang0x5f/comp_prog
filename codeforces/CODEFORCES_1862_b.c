23480d1797d213c55f078e97e9df7507111990cd
dang0x5f <d4n9xcv@gmail.com>
2025-06-21 22:44:59 -0400

    1862_b . tried to optimize by removing a for-loop. did  not improve time.

--- codeforces/CODEFORCES_1862_b.c
+++ codeforces/CODEFORCES_1862_b.c
@@ -4,17 +4,16 @@ void solve(void)
 {
     int n;
     scanf("%d",&n)&&getchar();
-    int a[n],i=0;
+    int a[n],i=0,ei=0,pre=0;
     do{
-        scanf("%d",&a[i++]);
+        scanf("%d",&a[i]);
+        if(i!=0 && a[i]<pre) ++ei;
+        pre=a[i];
+        ++i;
     }while(getchar()!='\n');
 
-    for(int j=1; j<n; ++j){
-        if(a[j]<a[j-1]) ++i;
-    }
-
-    printf("%d\n%d ",i,a[0]);
-    int pre=a[0];
+    printf("%d\n%d ",i+ei,a[0]);
+    pre=a[0];
     for(int j=1; j<n; ){
         if(a[j]<pre){
             pre = a[j];
