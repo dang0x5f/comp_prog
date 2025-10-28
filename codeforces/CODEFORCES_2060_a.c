32b7d892f533ad880dc0577ec8bca62d0d91f63e
dang0x5f <d4n9xcv@gmail.com>
2025-10-27 22:03:59 -0400

    https://codeforces.com/problemset/problem/2060/A

--- codeforces/CODEFORCES_2060_a.c
+++ codeforces/CODEFORCES_2060_a.c
@@ -5,7 +5,43 @@
 #define SZ 100
 #define NFIBS 4
 
+#define ARRSIZE 5
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
 void solve(void)
+{
+    int a[ARRSIZE], count=0, max=0;
+    for(int i=0; i<ARRSIZE; ++i){
+        if(i==2)
+            continue;
+        else
+            scanf("%d",&a[i]);
+    }
+
+    a[2] = a[4]-a[3];
+    for(int i=0; i<3; ++i)
+        if(a[i+2]==(a[i+1]+a[i]))
+            ++count;    
+    max = MAX(max,count);
+
+    count=0;
+    a[2] = a[3]-a[1];
+    for(int i=0; i<3; ++i)
+        if(a[i+2]==(a[i+1]+a[i]))
+            ++count;
+    max = MAX(max,count);
+    
+    count=0;
+    a[2] = a[0]+a[1];
+    for(int i=0; i<3; ++i)
+        if(a[i+2]==(a[i+1]+a[i]))
+            ++count;
+    max = MAX(max,count);
+    
+    printf("%d\n",max);
+}
+
+void solve2(void)
 {
     int a[NFIBS],uniq[SZ+1]={0},cnt=0;
     for(int i=0; i<NFIBS; ++i) scanf("%d",&a[i]);
